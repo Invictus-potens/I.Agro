764a8e67a80718bbb11a00a433bd97983c729658
ettore.baldo <ettore.baldo@sou.unaerp.edu.br>
2026-02-16 16:45:45 -0300

    feat(api): implementar API Express + Sequelize CRUD e documentação Swagger

---
+++ app/api/swagger.js
@@ -0,0 +1,21 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Farmer\'s Companion API',
+      version: '1.0.0',
+      description: 'API CRUD para gerenciar dados de previsão de tempo e localidades agrícolas'
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Local server'
+      }
+    ]
+  },
+  apis: ['./server.js']
+};
+
+module.exports = swaggerJsdoc(options);
