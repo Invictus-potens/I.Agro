ea5d7c1e446ba142a4850eba1e0cc01b40e2dab4
Invictus-potens <felipeommelo@gmail.com>
2026-04-10 11:51:43 -0300

    feat: Atualizado o container para fazer um proxy na url do backend para trazer os dados para o frontend.

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  async rewrites() {
+    const backendUrl = process.env.BACKEND_URL || 'http://localhost:8005';
+    return [
+      {
+        source: '/api-proxy/:path*',
+        destination: `${backendUrl}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
